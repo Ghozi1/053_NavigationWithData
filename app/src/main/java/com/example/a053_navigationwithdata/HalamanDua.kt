2726ac3e949681c93d8148426c34c242925d76eb
Ghozi <mghozighifar04@gmail.com>
2023-11-15 13:22:24 +0700

    Membuat Tampilan HalamanDua

--- app/src/main/java/com/example/a053_navigationwithdata/HalamanDua.kt
+++ app/src/main/java/com/example/a053_navigationwithdata/HalamanDua.kt
@@ -1,5 +1,7 @@
 package com.example.a053_navigationwithdata
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -14,12 +16,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.a053_navigationwithdata.data.OrderUIState
 import com.example.a053_navigationwithdata.ui.theme.komponen.FormatLabelHarga
-
 @Composable
 fun HalamanDua(
     orderUiState: OrderUIState,
@@ -38,32 +42,81 @@ fun HalamanDua(
             modifier = Modifier.padding(dimensionResource(R.dimen.padding_medium)),
             verticalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_small))
         ) {
-            items.forEach { item ->
-                Column {
-                    Text(item.first.uppercase())
-                    Text(text = item.second.toString(), fontWeight = FontWeight.Bold)
+            Column(
+                modifier = Modifier
+                    .padding(10.dp),
+
+                ) {
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.spacedBy(20.dp)
+                ) {
+                    Text(stringResource(id = R.string.nama))
+                    Text(text = "contactUIState.nama")
                 }
-                Divider(thickness = dimensionResource(R.dimen.thickness_divider))
+                Divider()
+                Spacer(modifier = Modifier.padding(16.dp))
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.spacedBy(20.dp)
+                ) {
+                    Text(stringResource(id = R.string.no_hp))
+                    Text(text = "contactUIState.noTelp")
+                }
+                Divider()
+                Spacer(modifier = Modifier.padding(16.dp))
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.spacedBy(20.dp)
+                ) {
+                    Text(stringResource(id = R.string.alamat))
+                    Text(text = "contactUIState.alamat")
+                }
+                Divider()
+                Spacer(modifier = Modifier.padding(16.dp))
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.Center,
+                ) {
+                    Text(
+                        text = "PESANAN ANDA", fontWeight = FontWeight.Bold, modifier = Modifier
+                            .border(
+                                BorderStroke(2.dp, Color.Cyan)
+                            )
+                            .padding(10.dp)
+                    )
+                }
+                Spacer(modifier = Modifier.padding(16.dp))
+                items.forEach { item ->
+                    Column {
+                        Text(item.first.uppercase())
+                        Text(text = item.second.toString(), fontWeight = FontWeight.Bold)
+                    }
+                    Divider(thickness = dimensionResource(R.dimen.thickness_divider))
+                }
+                Spacer(modifier = Modifier.height(dimensionResource(id = R.dimen.padding_small)))
+                FormatLabelHarga(
+                    subtotal = orderUiState.harga,
+                    modifier = Modifier.align(Alignment.End)
+                )
             }
-            Spacer(modifier = Modifier.height(dimensionResource(id = R.dimen.padding_small)))
-            FormatLabelHarga(
-                subtotal = orderUiState.harga,
-                modifier = Modifier.align(Alignment.End)
-            )
-        }
-        Row(
-            modifier = Modifier
-                .weight(1f, false)
-                .padding(dimensionResource(R.dimen.padding_small))
-        ) {
-            Column (
-                verticalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_small))
-            ){
-                Button(modifier = Modifier.fillMaxWidth(),onClick = {}) {
-                    Text(stringResource(R.string.send))
-                }
-                OutlinedButton(modifier = Modifier.fillMaxWidth(),onClick = onCancelButtonClicked) {
-                    Text(stringResource(R.string.cancel))
+            Row(
+                modifier = Modifier
+                    .weight(1f, false)
+                    .padding(dimensionResource(R.dimen.padding_small))
+            ) {
+                Column(
+                    verticalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_small))
+                ) {
+                    Button(modifier = Modifier.fillMaxWidth(), onClick = {}) {
+                        Text(stringResource(R.string.send))
+                    }
+                    OutlinedButton(
+                        modifier = Modifier.fillMaxWidth(),
+                        onClick = onCancelButtonClicked
+                    ) {
+                        Text(stringResource(R.string.cancel))
+                    }
                 }
             }
         }
